dd0cec0a58b9dc655abc0223699de3611f028b9d
AliAbdallah454 <alikhaledabdallah454@gmail.com>
2024-11-28 21:10:31 +0200

    added consistent_hashing_server

---
+++ consisten_hashing_server/src/main.rs
@@ -0,0 +1,64 @@
+#[macro_use] extern crate rocket;
+
+use std::hash::DefaultHasher;
+use std::sync::Mutex;
+
+use my_consistent_hashing::transaction::Transaction;
+use rocket::serde::{json::Json, Deserialize, Serialize};
+use rocket::State;
+use my_consistent_hashing::consistent_hashing::ConsistentHashing;
+
+#[derive(Deserialize)]
+struct Input {
+    value: String,
+}
+
+#[derive(Serialize)]
+struct Output {
+    input: String,
+    node: String,
+}
+
+#[post("/add", format = "json", data = "<input>")]
+fn get_node(input: Json<Input>, ring: &State<Mutex<ConsistentHashing<DefaultHasher>>>) -> Json<Output> {
+    let input_value = input.value.clone();
+    println!("Input val is: {}", input_value);
+    let ring = ring.lock().expect("Failed to lock the consistent hashing ring");
+    let node = ring.get_node(&input_value).unwrap().to_string();
+
+
+
+    Json(Output {
+        input: input_value,
+        node,
+    })
+}
+
+#[post("/remove-node", format = "json", data = "<input>")]
+fn remove_node(input: Json<Input>, ring: &State<Mutex<ConsistentHashing<DefaultHasher>>>) -> Json<Vec<Transaction>> {
+    let input_value = input.value.clone();
+    println!("Removing node {}", input_value);
+    let mut ring = ring.lock().expect("Failed to lock the consistent hashing ring");
+    let transactions = ring.remove_node(&input_value).unwrap();
+    return Json(transactions);
+}
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    let mut ring = ConsistentHashing::<DefaultHasher>::new(2);
+    for i in 0..3 {
+        ring.add_node(format!("127.0.0.1:900{}", i).as_str()).unwrap();
+    }
+    println!("Initialized ring");
+
+    let ring = Mutex::new(ring);
+
+    rocket::build()
+        .manage(ring)
+        .mount("/", routes![hello, get_node, remove_node])
+}
