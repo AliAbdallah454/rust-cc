849b282465939e6a48944d36d0b46e6f8c0e5062
AliAbdallah454 <alikhaledabdallah454@gmail.com>
2024-12-07 10:27:18 +0200

    eddited add_node to check for status codes

--- consistent_hashing_server/src/main.rs
+++ consistent_hashing_server/src/main.rs
@@ -54,8 +54,9 @@ async fn add_task(input: Json<TaskInfo>, ecs: &State<aws_sdk_ecs::Client>) -> St
 async fn add_node(input: Json<Input>, ring: &State<Mutex<ConsistentHashing>>) -> Json<Value> {
 
     let input_value = input.value.clone();
-    println!("adding node with ip: {}", input_value);
+    println!("Adding node with ip: {}", input_value);
     let mut ring = ring.lock().expect("Failed to lock the consistent hashing ring");
+    
     let transactions = match ring.add_node(&input_value) {
         Ok(transactions) => transactions,
         Err(e) => {
@@ -69,8 +70,9 @@ async fn add_node(input: Json<Input>, ring: &State<Mutex<ConsistentHashing>>) ->
 
     drop(ring);
 
-    for t in &transactions {
-        println!("{:?}", t);
+    println!("Transactions to be made: ");
+    for transaction in &transactions {
+        println!("{:?}", transaction);
     }
 
     let mut groups = HashMap::new();
@@ -86,32 +88,63 @@ async fn add_node(input: Json<Input>, ring: &State<Mutex<ConsistentHashing>>) ->
     for (key, transactions) in groups {
         tokio::spawn(async move {
 
+            println!("Current transactions: ");
+            for transaction in &transactions {
+                println!("{:?}", transaction);
+            }
+
             let source = &key.0;
             let destination = &key.1;
 
             let client = reqwest::Client::new();
 
             let source_uri = format!("http://{source}:7000/transactions");
-            // let source_uri = format!("http://{source}/transactions");
             let get_response = client.post(&source_uri).json(&transactions).send().await.expect("this should not panic");
+
+            if get_response.status() != 200 {
+                println!("Unable to get exclusive");
+                return;
+            }
+
             let data = get_response.text().await.unwrap();
             let json_data: Value = serde_json::from_str(&data).unwrap();
-
             let (exclusive, token): (Value, String) = serde_json::from_value(json_data).unwrap();
+
             println!("{:?}", exclusive);
-            println!("{}", token);
             
             let destination_uri = format!("http://{destination}:7000/exclusive");
-            // let destination_uri = format!("http://{destination}/exclusive");
-            let post_response = client.post(&destination_uri).json(&exclusive).send().await.expect("this should not panic");
+            let mut post_response = None;
+            for _ in 0..2 {
+                post_response = Some(client.post(&destination_uri).json(&exclusive).send().await);
+                if post_response.as_ref().unwrap().is_ok() {
+                    break;
+                }
+                println!("Connection refused for {}, retrying", &destination);
+                tokio::time::sleep(tokio::time::Duration::from_millis(250)).await;
+            }
+
+            let post_response = post_response.unwrap().unwrap();
+            if post_response.status() != 200 {
+                println!("Exclusive wasn't ingested");
+                return
+            }
+            println!("{:?}", post_response);
+
             let post_data = post_response.text().await.unwrap();
+            println!("data: {}", post_data);
 
             let delete_uri = format!("http://{source}:7000/delete-batch/{}", &token);
+            let delete_response = client.post(delete_uri).send().await.unwrap();
 
-            let delete_response = client.post(delete_uri).send().await;
+            if delete_response.status() != 200 {
+                println!("Data may have not been delete");
+                return;
+            }
+
+            let delete_data = delete_response.text().await.unwrap();
+            println!("{}", delete_data);
 
         });
-
     }
     
     return Json(serde_json::json!({
